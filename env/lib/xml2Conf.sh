578b79c348b26cc3a1060cab98fe214f62eb4495
donib-irakihda <donib.irakihda@gmail.com>
2022-01-30 14:33:39 +0545

    Added my project

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/donib/Desktop/heart-disease-project/env/lib"
+XML2_LIBS="-lxml2 -L/home/donib/Desktop/heart-disease-project/env/lib -lz -L/home/donib/Desktop/heart-disease-project/env/lib -llzma -lpthread -L/home/donib/Desktop/heart-disease-project/env/lib -L/home/donib/Desktop/heart-disease-project/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/home/donib/Desktop/heart-disease-project/env/include/libxml2 -I/home/donib/Desktop/heart-disease-project/env/include"
+MODULE_VERSION="xml2-2.9.12"
+
